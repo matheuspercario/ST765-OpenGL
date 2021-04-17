c7d40318ff4641146263a932b72bf79703a4d8ba
Matheus Percário <matheuspercario@gmail.com>
2021-04-16 22:00:37 -0300

    Rocket draw

--- Rocket/rocket.cpp
+++ Rocket/rocket.cpp
@@ -10,7 +10,7 @@ void keyboard(unsigned char key, int x, int y);
 int main(int argc, char** argv){
   glutInit(&argc, argv);                         // inicializa o glut
   glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);  // especifica uso de cores e buffers
-  glutInitWindowSize (600, 600);                 // especifica as dimensoes da janela
+  glutInitWindowSize (900, 600);                 // especifica as dimensoes da janela
   glutInitWindowPosition (100, 100);             // especifica aonde a janela aparece na tela
   glutCreateWindow ("Desenhando um Foguete");     // cria a janela
   init();
@@ -24,7 +24,7 @@ int main(int argc, char** argv){
 
 void init(void){
   glClearColor(0.0, 0.0, 0.0, 1.0);    // cor de fundo 
-  glOrtho(0, 600, 0, 600, -1 ,1);      // modo de projecao ortogonal
+  glOrtho(0, 900, 0, 600, -1 ,1);      // modo de projecao ortogonal
 } 
 
 void display(void)
@@ -32,15 +32,74 @@ void display(void)
   int i;
   glClear(GL_COLOR_BUFFER_BIT);               // limpa a janela
   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);  // NOTHING = Preenchido
-  glColor3f (0.0, 1.0, 0.0);                  // cor da linha
-  glLineWidth(4.0); 
+  glColor3f (0.0, 0.0, 1.0);                  // cor da linha
+  glLineWidth(2.0); 
   glBegin(GL_TRIANGLE_STRIP);
-  glVertex2i(0, 50); glVertex2i(100, 150); glVertex2i(50, 200); 
-				  						   glVertex2i(100, 250);
-
-  
-  
-
+  glVertex2i(800, 400);
+  glVertex2i(900, 300); 
+  glVertex2i(800, 300); 
+  glVertex2i(800, 200);
+  glVertex2i(700, 300);
+  glVertex2i(700, 200);
+  glVertex2i(700, 250);
+  glVertex2i(650, 200);
+  glVertex2i(650, 250);
+  glVertex2i(600, 200);
+  glVertex2i(600, 250);
+  glVertex2i(600, 350);
+  glVertex2i(600, 200);
+  glVertex2i(450, 200);
+  glVertex2i(450, 200);
+  glVertex2i(450, 250);
+  glVertex2i(400, 200);
+  glVertex2i(400, 250);
+  glVertex2i(350, 200);
+  glVertex2i(350, 250);
+  glVertex2i(300, 200);
+  glVertex2i(300, 250);
+  glVertex2i(300, 200);
+  glVertex2i(300, 200);
+  glVertex2i(100, 0);
+  glVertex2i(100, 200);
+  glVertex2i(0, 100);
+  glVertex2i(100, 200);
+  glVertex2i(100, 200);
+  glVertex2i(300, 400);
+  glVertex2i(100, 400);
+  glVertex2i(100, 600);
+  glVertex2i(0, 500);
+  glVertex2i(100, 400);
+  glVertex2i(100, 400);
+  glVertex2i(300, 400);
+  glVertex2i(100, 200);
+  glVertex2i(300, 200);
+  glVertex2i(300, 200);
+  glVertex2i(350, 250);
+  glVertex2i(300, 250);
+  glVertex2i(350, 300);
+  glVertex2i(300, 300);
+  glVertex2i(350, 350);
+  glVertex2i(300, 350);
+  glVertex2i(300, 400);
+  glVertex2i(300, 400); 
+  glVertex2i(350, 350); 
+  glVertex2i(350, 400);  
+  glVertex2i(400, 350); 
+  glVertex2i(400, 400);
+  glVertex2i(450, 350);
+  glVertex2i(450, 400);
+  glVertex2i(450, 400);
+  glVertex2i(450, 200);
+  glVertex2i(650, 400);
+  glVertex2i(600, 350);
+  glVertex2i(700, 350);
+  glVertex2i(650, 400);
+  glVertex2i(700, 400);
+  glVertex2i(700, 400);
+  glVertex2i(700, 300);
+  glVertex2i(800, 400);
+  glVertex2i(700, 300);
+  glVertex2i(800, 300);
   glEnd();
   glFlush();
 }
